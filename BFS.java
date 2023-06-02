808ece9a0c65a0cc87e1a6c5ba8f9d491ea5dd5b
Deepak kumar <dkp0043@gmail.com>
2023-06-02 21:39:38 +0530

    Graph_1

---
+++ BFS.java
@@ -0,0 +1,68 @@
+import java.util.*;
+
+public class BFS
+{
+
+    public static class Edge{
+        int src;
+        int  dest;
+       // int weight;
+
+        public Edge(int s,int d)
+        {
+            this.src=s;
+            this.dest=d;
+           // this.weight=w;
+        }
+    }
+
+    public static void createG(ArrayList<Edge> graph[])
+    {
+        for(int i=0;i<graph.length;i++)
+        {
+            graph[i]=new ArrayList<Edge>(i);
+        }
+        graph[0].add(new Edge(0, 1));
+        graph[0].add(new Edge(0, 2));
+        graph[1].add(new Edge(1, 0));
+        graph[1].add(new Edge(1, 3));
+        graph[2].add(new Edge(2, 0));
+        graph[2].add(new Edge(2, 4));
+        graph[3].add(new Edge(3, 1));
+        graph[3].add(new Edge(3, 4));
+        graph[3].add(new Edge(3, 5));
+        graph[4].add(new Edge(4, 2));
+        graph[4].add(new Edge(4, 3));
+        graph[4].add(new Edge(4, 5));
+        graph[5].add(new Edge(5, 3));
+        graph[5].add(new Edge(5, 4));
+        graph[5].add(new Edge(5, 6));
+        graph[6].add(new Edge(6, 5));
+    }
+
+    public static void bFS(ArrayList<Edge> graph[],int V){
+        Queue<Integer>q=new LinkedList<>();
+        boolean vist[]=new boolean[V];
+        q.add(0);
+        while(!q.isEmpty()){
+            int curr=q.remove();
+            if(vist[curr]==false){
+                System.out.print(curr+" ");
+                vist[curr]=true;
+
+                for(int i=0;i<graph[curr].size();i++){
+                    Edge e=graph[curr].get(i);
+                    q.add(e.dest);
+                }
+            }
+        }
+    }
+    public static void main(String[] args)
+    {
+        int V=7;
+        ArrayList<Edge> graph[]=new ArrayList[V];
+        createG(graph);
+        bFS(graph,V);
+        System.out.println();
+    }
+}
